2519d3abede24cc3b05cebd8d36b5391f32b442c
sebastien <sebastien.jacquot97@gmail.com>
2021-10-16 15:53:40 +0200

    connexion utilisateur

--- webpack.config.js
+++ webpack.config.js
@@ -21,6 +21,8 @@ Encore
      * and one CSS file (e.g. app.scss) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addEntry('page', './assets/page.js')
+
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
