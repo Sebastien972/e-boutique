cd5a32c8be33f742cecf7bc77f57299058f1917b
sebastien <sebastien.jacquot97@gmail.com>
2021-12-02 12:04:49 +0100

    update back-office

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -11,10 +12,13 @@ class HomeController extends AbstractController
     /**
      * @Route("/", name="home")
      */
-    public function index(): Response
+    public function index(ProduitRepository $produitRepository): Response
     {
+        $produit = $produitRepository->findAll();
+        $meilleurVente =$produitRepository->findByMeilleurVente(1);
+            dd([$produit, $meilleurVente]);
         return $this->render('home/index.html.twig', [
-            'controller_name' => 'HomeController',
+            'meilleurVente' => $meilleurVente,
         ]);
     }
 }
