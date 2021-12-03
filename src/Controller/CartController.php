c85a5e4747d0e3f27001fcabd82baf5ab44bed68
sebastien <sebastien.jacquot97@gmail.com>
2021-12-03 17:32:13 +0100

    init panier avec la session

--- src/Controller/CartController.php
+++ src/Controller/CartController.php
@@ -2,22 +2,60 @@
 
 namespace App\Controller;
 
+use App\Services\CartServices;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpFoundation\Session\SessionInterface;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class CartController
+ * @package App\Controller
+ */
 class CartController extends AbstractController
 {
     /**
      * @Route("/cart", name="cart")
      */
-    public function index(SessionInterface $session): Response
+    public function index(CartServices $cartServices): Response
     {
-        $session->set('cart', ['name'=>'session']);
-        $cart = $session->get('cart');
+        $cart = $cartServices->getFullCart();
+
+        $total =  $cartServices->getTotalCart();
+
+
         return $this->render('cart/index.html.twig', [
             'cart' => $cart,
+            'total'=> $total,
         ]);
+
+
     }
+
+    /**
+     * @Route("/add/{id}", name="add", requirements={"id"="\d+"})
+     * @param $id
+     */
+    public function panier($id, CartServices $cartServices)
+    {
+
+        $cartServices->add($id);
+
+
+        return $this->redirectToRoute('cart');
+
+
+
+    }
+    /**
+     * @Route("/remove/{id}", name="remove", requirements={"id"="\d+"})
+     */
+    public function removpanier($id, CartServices $cartServices)
+    {
+
+        $cartServices->remove($id);
+
+        return $this->redirectToRoute('cart');
+
+    }
+
 }
