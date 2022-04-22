8f015a531b3e783d409a04a222150652cad7d434
sebastien <sebastien.jacquot97@gmail.com>
2022-04-22 05:02:36 +0200

    administation des commande et afiche des commandes passer

--- src/Controller/UserAccount/AccountUserController.php
+++ src/Controller/UserAccount/AccountUserController.php
@@ -2,7 +2,9 @@
 
 namespace App\Controller\UserAccount;
 
+use App\Entity\Commande;
 use App\Repository\AdresseRepository;
+use App\Repository\CommandeRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -17,10 +19,27 @@ class AccountUserController extends AbstractController
     /**
      * @Route("/user", name="account_user")
      */
-    public function index(AdresseRepository $adresseRepository): Response
+    public function index(AdresseRepository $adresseRepository,CommandeRepository $commandeRepository): Response
     {
+
+        $commande = $commandeRepository->findBy(['isPaid'=> true, 'user'=>$this->getUser()]);
+        // dd($commande);
         return $this->render('account_user/index.html.twig' , [
             'adresse' => $adresseRepository->findAll(),
+            'commande'=> $commande,
+        ]);
+    }
+    /**
+     * @Route("/user/commande{id}", name="commande_user")
+     */
+    public function commande(?Commande $commande)
+    {
+        if (!$commande || $this->getUser() !== $commande->getUser() || !$commande->getIsPaid) {
+            return $this->redirectToRoute('home');
+        }
+        // dd($commande);
+        return $this->render('account_user/commande.html.twig' , [
+            'commande'=> $commande,
         ]);
     }
 }
